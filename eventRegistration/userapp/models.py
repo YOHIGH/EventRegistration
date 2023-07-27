8d4cfa20cd74154fcb6c0f3d21ffe2d1d54f85ce
shubhamjain1 <shubham.jain431@gmail.com>
2023-07-27 14:41:28 -0400

    27th July 2023 [AbstractUser Created]

--- eventRegistration/userapp/models.py
+++ eventRegistration/userapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    MALE = 'male'
+    FEMALE = 'female'
+    OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    )
+
+    # user = models.ForeignKey()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_photo/',default='test.png', blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    
+    def save(self, *args, **kwargs):
+        # import pdb
+        # pdb.set_trace()
+        if self.gender == "male":
+            self.photo = "test.png"
+        
+        super().save(*args, **kwargs)
